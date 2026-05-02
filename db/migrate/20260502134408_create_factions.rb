ef84c226e5c6f33d6aee7dbbb6f60654146688ce
Serge <s@shayderov.ru>
2026-05-02 21:51:31 +0300

    Linter fixes

--- db/migrate/20260502134408_create_factions.rb
+++ db/migrate/20260502134408_create_factions.rb
@@ -1,6 +1,6 @@
 class CreateFactions < ActiveRecord::Migration[8.1]
   def change
-    create_table :factions do |t| 
+    create_table :factions do |t|
       t.string :name, null: false
 
       t.timestamps
