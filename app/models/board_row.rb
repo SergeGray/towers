9a457f50d3fc200fb0fbd0dbd7bc984300d53890
Serge <s@shayderov.ru>
2026-05-02 21:48:24 +0300

    Fix trailing whitespace

--- app/models/board_row.rb
+++ app/models/board_row.rb
@@ -3,7 +3,7 @@ class BoardRow < ApplicationRecord
 
   has_many :play_cards, as: :placeable, dependent: :destroy
 
-  validates :position, presence: true, 
+  validates :position, presence: true,
                        uniqueness: { scope: :board_id },
                        numericality: { greater_than_or_equal_to: 0 }
 end
