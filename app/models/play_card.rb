9bf21e9da9001c031582fccaa0af9e797da591f8
Serge <s@shayderov.ru>
2026-05-08 16:29:35 +0300

    Callbacks on play card creation

--- app/models/play_card.rb
+++ app/models/play_card.rb
@@ -2,4 +2,12 @@ class PlayCard < ApplicationRecord
   belongs_to :player
   belongs_to :card
   belongs_to :placeable, polymorphic: true
+
+  before_validation :before_validation_add_to_deck
+
+  private
+
+  def before_validation_add_to_deck
+    self.placeable ||= player.play_deck
+  end
 end
